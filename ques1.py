04d72ac8a92587136e0a606127c9211325b7836b
Shahnawaz Khan <mailtoshahnawazk@gmail.com>
2023-04-23 15:08:39 +0530

    Added results folder

--- ques1.py
+++ ques1.py
@@ -28,7 +28,7 @@ for optimiser in optimisers:
     for dataset,num_clients in zip(datasets, num_clients):
         for adversary in adverseries:
             for ratio in currupt_ratio:
-                filename = os.path.join(RESULTS_DIR, 'results') + dataset + '_' + optimiser + '_' + str(adversary) + '_' + str(ratio) + '.txt'
+                filename = os.path.join(RESULTS_DIR, 'results/') + dataset + '_' + optimiser + '_' + str(adversary) + '_' + str(ratio) + '.txt'
                 num_currupted = int(num_clients * ratio)
                 A = [0,0,0]
                 A[adversary-1] = 1
